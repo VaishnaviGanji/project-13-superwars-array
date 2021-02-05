d681ef4028e704e2599241dba56b2fa1321c6b17
VaishnaviGanji <vaishnaviganji24@gmail.com>
2021-02-05 20:35:34 +0530

    1578196

--- src/app.js
+++ src/app.js
@@ -25,8 +25,14 @@ const PLAYERS = [
 const initPlayers = (players) => {
     let detailedPlayers = '';
 
-    // Instead of forloop use Map method
-    // Code here
+    detailedPlayers= players.map((player, playerId) => {
+        return {
+            name: player,
+            strength: getRandomStrength(),
+            image: "images/super-" + (playerId + 1) + ".png",
+            type: (playerId % 2 == 0) ? 'hero' : 'villain',
+        }
+    });
 
     return detailedPlayers;
 }
@@ -43,6 +49,12 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    fragment = players.filter(player => player.type == type).map(player => 
+        `<div class="player">
+    <img src="${player.image}" alt = "">
+    <div class="name">${player.name}</div>
+    <div class="strength">${player.strength}</div>
+ </div>`).join();
 
     return fragment;
 }
